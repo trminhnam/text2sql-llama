8237527733f8baeb87565474737889e36abfaa4b
trminhnam <trminhnam20082002@gmail.com>
2023-09-08 17:54:31 +0700

    add api app

--- app.py
+++ app.py
@@ -9,16 +9,11 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from transformers import HfArgumentParser
 
-from utils.arguments import PredictArguments, TextToSqlTrainingArguments
-from utils.load_dataset import get_context_with_db_name
+from utils.arguments import PredictArguments
 from utils.load_model import load_model_with_peft_and_tokenizer
 from utils.prompter import generate_llama_prompt_sql, generate_prompt_sql
 
 load_dotenv()
-
-PORT = os.environ.get("PORT", 5000)
-DEBUG = os.environ.get("DEBUG", False)
-JSON_CONFIG_PATH = os.environ.get("JSON_CONFIG_PATH", "config.json")
 
 app = Flask(__name__)
 
@@ -74,10 +69,9 @@ def ask_text2sql():
     # Get the data from the POST request.
     data = request.get_json(force=True)
 
+    # get data from the request
     context = data["context"]
     question = data["question"]
-
-    # log data to the console so we can see what we received
 
     if PREDICT_ARGS.use_llama_prompt:
         prompt = generate_llama_prompt_sql(question, context)
@@ -107,6 +101,7 @@ if __name__ == "__main__":
     global MODEL, TOKENIZER, DEVICE, PREDICT_ARGS
 
     parser = HfArgumentParser((PredictArguments))
+    JSON_CONFIG_PATH = os.environ.get("JSON_CONFIG_PATH", "config.json")
     PREDICT_ARGS = parser.parse_json_file(json_file=JSON_CONFIG_PATH)[0]
 
     print(f"Loading app with arguments {PREDICT_ARGS}")
@@ -114,6 +109,8 @@ if __name__ == "__main__":
     MODEL, TOKENIZER = load_model_with_peft_and_tokenizer(PREDICT_ARGS, None)
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    PORT = os.environ.get("PORT", 8899)
+    DEBUG = os.environ.get("DEBUG", False)
     app.run(
         host="0.0.0.0",
         port=PORT,
