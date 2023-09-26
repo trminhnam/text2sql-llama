0bbd3795e489c9565e326a6a118a251eb8ab8c38
trminhnam <tmnam20@apcs.fitus.edu.vn>
2023-09-26 14:39:55 +0700

    chore: update predict with ggml

--- predict.py
+++ predict.py
@@ -9,10 +9,23 @@ import torch
 from tqdm.auto import tqdm
 from transformers import HfArgumentParser
 
-from utils.arguments import PredictArguments, TextToSqlTrainingArguments
+from utils.arguments import (
+    PredictArguments,
+    TextToSqlTrainingArguments,
+    LlamaCppArguments,
+)
 from utils.load_dataset import creating_schema, get_context_with_db_name
-from utils.load_model import load_model_with_peft_and_tokenizer
+from utils.load_model import load_model_with_peft_and_tokenizer, load_llama_cpp_model
 from utils.prompter import generate_llama_prompt_sql, generate_prompt_sql
+from utils.timeout import Timeout, timeout
+
+try:
+    from llama_cpp import Llama
+
+    imported_llama_cpp = True
+except ImportError:
+    print("Failed to import Llama. Please install it first.")
+    imported_llama_cpp = False
 
 
 @torch.no_grad()
@@ -23,17 +36,38 @@ def predict(model, tokenizer, prompt, device="cuda", args={}):
     outputs = model.generate(
         input_ids=inputs["input_ids"],
         attention_mask=inputs["attention_mask"],
-        num_return_sequences=1,
+        # num_return_sequences=1,
+        max_length=args.max_length,
         max_new_tokens=args.max_new_tokens,
-        do_sample=args.do_sample,
-        num_beams=args.num_beams,
+        # do_sample=args.do_sample,
+        # num_beams=args.num_beams,
         top_k=args.top_k,
         top_p=args.top_p,
         temperature=args.temperature,
+        max_time=args.max_time,
     )
     return tokenizer.batch_decode(
         outputs.detach().cpu().numpy(), skip_special_tokens=True
     )[0]
+
+
+def llama_cpp_predict(model, prompt, args={}):
+    if args.max_new_tokens is not None:
+        prompt_n_tokens = len(model.tokenize(prompt.encode("utf-8")))
+        max_tokens = args.max_new_tokens + prompt_n_tokens
+    else:
+        max_tokens = args.max_length
+
+    model_output = model(
+        prompt,
+        echo=True,
+        max_tokens=max_tokens,
+        top_k=args.top_k,
+        top_p=args.top_p,
+        temperature=args.temperature,
+    )
+
+    return model_output["choices"][0]["text"]
 
 
 def preprocess_text(text):
@@ -66,11 +100,18 @@ if __name__ == "__main__":
     parser = HfArgumentParser(
         (
             PredictArguments,
+            LlamaCppArguments,
             TextToSqlTrainingArguments,
         )
     )
-    predict_args, training_args = parser.parse_args_into_dataclasses()
+    predict_args, llama_cpp_args, training_args = parser.parse_args_into_dataclasses()
     print(f"Predict args: {predict_args}")
+    print("#" * 50)
+    print()
+
+    print(f"Llama-cpp args: {llama_cpp_args}")
+    print("#" * 50)
+    print()
 
     # make output dirs
     base_dir = os.path.dirname(predict_args.output_path)
@@ -84,6 +125,12 @@ if __name__ == "__main__":
         save_dir = os.path.join(save_dir, f"beam_{predict_args.num_beams}")
     os.makedirs(save_dir, exist_ok=True)
     predict_args.output_path = os.path.join(save_dir, file_name)
+    if llama_cpp_args.llama_cpp_model_path:
+        model_filename = os.path.basename(llama_cpp_args.llama_cpp_model_path)
+        predict_args.output_path = os.path.join(
+            save_dir, model_filename + "." + file_name
+        )
+
     print(f"Output path to: {predict_args.output_path}")
 
     # load spider dataset: schema, primary key, foreign key
@@ -95,11 +142,24 @@ if __name__ == "__main__":
     dev_dataset = pd.read_json(os.path.join(predict_args.dataset_dir, "dev.json"))
 
     # load model and tokenizer with path
-    model, tokenizer = load_model_with_peft_and_tokenizer(predict_args, training_args)
-    device = (
-        "cuda" if (torch.cuda.is_available() and not training_args.no_cuda) else "cpu"
-    )
-    model.to(device)
+    if llama_cpp_args.llama_cpp_model_path:
+        # load model and tokenizer instead of loading with default function
+        model = Llama(
+            model_path=llama_cpp_args.llama_cpp_model_path,
+            n_ctx=llama_cpp_args.n_ctx,
+            n_gpu_layers=llama_cpp_args.n_gpu_layers,
+        )
+        print(f"Use llama-cpp model from {llama_cpp_args.llama_cpp_model_path}")
+    else:
+        model, tokenizer = load_model_with_peft_and_tokenizer(
+            predict_args, training_args
+        )
+        device = (
+            "cuda"
+            if (torch.cuda.is_available() and not training_args.no_cuda)
+            else "cpu"
+        )
+        model.to(device)
 
     predictions = []
     for idx, row in tqdm(dev_dataset.iterrows(), total=len(dev_dataset)):
@@ -112,13 +172,26 @@ if __name__ == "__main__":
             prompt = generate_llama_prompt_sql(question, context)
         else:
             prompt = generate_prompt_sql(question, context)
+        try:
+            if llama_cpp_args.llama_cpp_model_path:
+                prediction = llama_cpp_predict(model, prompt, args=predict_args)
+            else:
+                prediction = predict(
+                    model, tokenizer, prompt, device, args=predict_args
+                )
 
-        prediction = predict(model, tokenizer, prompt, device, args=predict_args)
-
-        if predict_args.use_llama_prompt:
-            prediction = get_llama_sql_statement(prediction)
-        else:
-            prediction = get_sql_statement(prediction)
+            if predict_args.use_llama_prompt:
+                prediction = get_llama_sql_statement(prediction)
+            else:
+                prediction = get_sql_statement(prediction)
+        except Exception as e:
+            print(f"Failed to predict {idx}-th question")
+            print(f"Question: {question}")
+            print(f"Context: {context}")
+            print(f"Error: {e}")
+            print("*" * 50)
+            print()
+            prediction = ""
 
         predictions.append(prediction)
 
